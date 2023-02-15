32d5f2cd87b9b957c4881d74da901c164191dc30
Siddhartha <siddharthabhatt2345@gmail.com>
2023-02-15 21:45:42 +0530

    Added Connect Wallet Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import metamask from './images/metamask.png';
+import {ethers} from 'ethers';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [currentAccount, setCurrentAccount] = useState('');
+  const [correctNetwork, setCorrectNetwork] = useState(false);
+
+  const connectWallet = async () => {
+    try{
+      const {ethereum} = window;
+      if(!ethereum){
+        alert('Metamask Not Found ! Get MetaMask and Try Again.');
+        return;
+      }
+
+      let chainId = await ethereum.request({method: 'eth_chainId'});
+
+      const gorlieChainId = '0x5';
+      console.log(chainId);
+      if(chainId !== gorlieChainId){
+        alert('Please Connect to Gorlie Testnet');
+        return;
+      }
+      else{
+        setCorrectNetwork(true);
+      }
+
+      const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+      setCurrentAccount(accounts[0]);
+      console.log('Connected', accounts[0]);
+    } catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    connectWallet();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {currentAccount === '' ? (
+          <div className="loading" style={{width:"100%",height:"100vh",display:'flex',alignItems:"center", justifyContent:"space-evenly",flexDirection:"column"}}>
+            <div style={{display:'flex',alignItems:"center", justifyContent:"center",flexDirection:"column"}}>
+              <div className="Metabit">Metabit</div>
+              <div className="SubMetabit">Empowering IoT With Blockchain</div>
+            </div>
+            <div style={{display:'flex',alignItems:"center", justifyContent:"center",flexDirection:"column", textAlign:"center"}}>
+              <div className='connectWalletButton' onClick={connectWallet}>
+                Login With <img src={metamask} alt="metamask"/> MetaMask
+              </div>
+              <div className="SubConnectWallet">Register</div>
+            </div>
+          
+          </div>
+          ) : correctNetwork ? (
+            <div className="App">
+              Add Next Page Here
+            </div>
+          ) : (
+          <div className='flex flex-col justify-center items-center mb-20 font-bold text-2xl gap-y-3'>
+          <div>-----------------------------------------</div>
+          <div>Please connect to the Gorlie Testnet</div>
+          <div>and reload the page</div>
+          <div>-----------------------------------------</div>
+          </div>
+        )}
     </div>
   );
 }
