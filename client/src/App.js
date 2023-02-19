4b0f2c348d870a7312ae70ebef13764bf7ba9703
FirstThinker_Swapnil <swapnilsinha81@gmail.com>
2023-02-19 13:36:15 +0530

    JS Functions : Components

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import metamask from './images/metamask.png';
 import {ethers} from 'ethers';
 import React, {useState, useEffect} from 'react';
 
+// import { Route } from "react-router-dom";
+import Functionality from './components/Functionality';
+
 function App() {
 
   const [currentAccount, setCurrentAccount] = useState('');
@@ -41,34 +44,39 @@ function App() {
   }, []);
 
   return (
-    <div>
-      {currentAccount === '' ? (
-          <div className="loading" style={{width:"100%",height:"100vh",display:'flex',alignItems:"center", justifyContent:"space-evenly",flexDirection:"column"}}>
-            <div style={{display:'flex',alignItems:"center", justifyContent:"center",flexDirection:"column"}}>
-              <div className="Metabit">Metabit</div>
-              <div className="SubMetabit">Empowering IoT With Blockchain</div>
-            </div>
-            <div style={{display:'flex',alignItems:"center", justifyContent:"center",flexDirection:"column", textAlign:"center"}}>
-              <div className='connectWalletButton' onClick={connectWallet}>
-                Login With <img src={metamask} alt="metamask"/> MetaMask
+    <>
+      <div>
+        {currentAccount === '' ? (
+            <div className="loading" style={{width:"100%",height:"100vh",display:'flex',alignItems:"center", justifyContent:"space-evenly",flexDirection:"column"}}>
+              <div style={{display:'flex',alignItems:"center", justifyContent:"center",flexDirection:"column"}}>
+                <div className="Metabit">Metabit</div>
+                <div className="SubMetabit">Empowering IoT With Blockchain</div>
               </div>
-              <div className="SubConnectWallet">Register</div>
+              <div style={{display:'flex',alignItems:"center", justifyContent:"center",flexDirection:"column", textAlign:"center"}}>
+                <div className='connectWalletButton' onClick={connectWallet}>
+                  Login With <img src={metamask} alt="metamask"/> MetaMask
+                </div>
+                <div className="SubConnectWallet">Register</div>
+              </div>
+            
             </div>
-          
-          </div>
-          ) : correctNetwork ? (
-            <div className="App">
-              Add Next Page Here
+            ) : correctNetwork ? (
+              <div className="App">
+                Add Next Page Here
+              </div>
+            ) : (
+            <div className='flex flex-col justify-center items-center mb-20 font-bold text-2xl gap-y-3'>
+            <div>-----------------------------------------</div>
+            <div>Please connect to the Gorlie Testnet</div>
+            <div>and reload the page</div>
+            <div>-----------------------------------------</div>
             </div>
-          ) : (
-          <div className='flex flex-col justify-center items-center mb-20 font-bold text-2xl gap-y-3'>
-          <div>-----------------------------------------</div>
-          <div>Please connect to the Gorlie Testnet</div>
-          <div>and reload the page</div>
-          <div>-----------------------------------------</div>
-          </div>
-        )}
-    </div>
+          )}
+
+          <Functionality />
+      </div>
+    {/* <Route path='/func' exact component={Functionality} /> */}
+    </>
   );
 }
 
---
+++ client/src/components/Weather.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Weather.css';
+
+function Weather(){
+    return(
+        <>
+            <h1 className="temp-value">30°C</h1>
+            <div className="icon"></div>
+        </>
+    )
+}
+
+export default Weather;
